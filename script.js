ecc5efa4f8c8f7208b059c9e0a2d8e3b56338a5d
Omidreza <144906057+OmidRezaWeb@users.noreply.github.com>
2024-09-08 21:30:43 +0330

    use setInterval for change background color

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-window.onload = function(){
-    let btn = document.getElementById('btn')
-    btn.addEventListener('click', function(e){
-        document.body.style.backgroundColor = randomeBgColor()
-    })
+// window.onload = function(){
+//     let btn = document.getElementById('btn')
+//     btn.addEventListener('click', function(e){
+//         document.body.style.backgroundColor = randomeBgColor()
+//     })
 
-    function randomeBgColor(){
-        return `rgb(${Math.floor(Math.random() *255)},${Math.floor(Math.random() *255)},${Math.floor(Math.random() *255)})`
-    }
+//     function randomeBgColor(){
+//         return `rgb(${Math.floor(Math.random() *255)},${Math.floor(Math.random() *255)},${Math.floor(Math.random() *255)})`
+//     }
+// }
+
+setInterval(flashBgColor, 500)
+function flashBgColor() {
+    document.body.style.background = `hsl(${Math.floor(Math.random() * 360)} ,100% ,50%)`
 }
